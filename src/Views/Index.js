5cb4b92f4d058829589a0e16630c5712632c241a
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-29 05:07:59 +0100

    Cooking...

---
+++ src/Views/Index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import UseCallback from "./UseCallback";
+import UseContext from "./UseContext";
+import UseEffect from "./UseEffect";
+import UseLayoutEffect from "./UseLayoutEffect";
+import UseMemo from "./UseMemo";
+import UseReducer from "./UseReducer";
+import UseRef from "./UseRef";
+import UseState from "./UseState";
+
+export { Home, UseCallback, UseContext, UseEffect, UseLayoutEffect, UseMemo, UseReducer, UseRef, UseState }
