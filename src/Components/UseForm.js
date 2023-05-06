4babc1330459fdb6aa5bc592b3bf48a1bc3a649e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-06 12:34:05 +0100

    Cooking...

--- src/Components/UseForm.js
+++ src/Components/UseForm.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export const UseForm = (initialValues) => {
-const [values, setValues] = useState(initialValues)
+  const [values, setValues] = useState(initialValues)
 
   return [
     values,
-    e => {setValues ({ ...values, [e.target.name]: e.target.value })}
+    e => { setValues({ ...values, [e.target.name]: e.target.value }) }
   ]
 }
--- src/Views/UseLayoutEffect.js
+++ src/Views/UseLayoutEffect.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { UseForm, UseMeasure } from '../Components/Index'
 import Index from '../Utilities/Index'
 
