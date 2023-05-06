07bf066a4758a23db16c32b042eb287826ca7563
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-06 12:08:51 +0100

    Cooking...

--- src/Utilities/Index.js
+++ src/Utilities/Index.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { useRef } from 'react'
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react'
+import { UseFetch } from '../Components/Index'
 
 const Index = () => {
-    const renders = useRef(0)
+    // const renders = useRef(0)
 
-    console.log(`hello renders: ${renders.current++}`)
+    // console.log(`hello renders: ${renders.current++}`)
+    const [count, setCount] = useState(() => JSON.parse(localStorage.getItem("count")))
+
+    const demoUrl = `http://numbersapi.com/${count}/trivia`
+
+    const { data, loading } = UseFetch(demoUrl)
+
+    useEffect(() => {
+        localStorage.setItem("count", JSON.stringify(count))
+    }, [count])
+
+    const [rect, setRect] = useState({})
+    const DivRef = useRef()
+
+    useLayoutEffect(() => {
+        setRect(DivRef.current.getBoundingClientRect())
+    }, [data])
 
     return (
-        <div>Index</div>
+        <div>
+            <div ref={DivRef}> {!data ? "loading..." : data} </div>
+            <pre>{JSON.stringify(rect, null, 2)}</pre>
+            <div>count: {count}</div>
+            <h4>Click for random fact about numbers</h4>
+            <button className='px-8 py-3 border border-black hover:text-gray-500 hover:border-gray-500 rounded-3xl' onClick={() => setCount(c => c + 1)}>click me</button>
+        </div>
     )
 }
 
