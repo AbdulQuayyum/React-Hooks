3a9d25f27e5b871b18020c18c158946409d2f773
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-06 13:16:59 +0100

    Steady Cooking👨🏾‍🍳...

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,8 +1,9 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import { Square } from "./Square"
 import { UseCountRenders } from "./UseCountRenders"
 import { UseFetch } from "./UseFetch"
 import { UseForm } from "./UseForm"
 import { UseMeasure } from "./UseMeasure"
 
-export { Footer, Navbar, UseCountRenders, UseFetch, UseForm, UseMeasure }
+export { Footer, Navbar, Square, UseCountRenders, UseFetch, UseForm, UseMeasure }
--- src/Views/UseCallback.js
+++ src/Views/UseCallback.js
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Hello } from '../Utilities/Hello'
+import { Square } from '../Components/Index'
 
 const UseCallback = () => {
-  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem("count")))
+  const [count, setCount] = useState(0)
+  const randomNumbers = [1, 2, 3, 4, 5]
+
+  // const increment = useCallback(() => {
+  //   setCount(c => c + 1)
+  // }, [setCount])
+
+  const increment = useCallback((n) => {
+    setCount(c => c + n)
+  }, [setCount])
 
   return (
     <div className='flex flex-col items-center justify-center w-full gap-y-4 '>
       <h4> UseCallback </h4>
       <p>
         useCallback is a React hook that memoizes a function so that it only changes when its dependencies change. This can be useful for optimizing performance by preventing unnecessary re-renders of components that depend on the function. You pass in the function and an array of dependencies, and the hook returns a memoized version of the function that can be passed as a prop to child components without causing unnecessary re-renders.</p>
-      <Hello increment={() => setCount(c => c + 1)} />
+      <Hello increment={increment} />
       <div>count: {count}</div>
+      {randomNumbers.map(n => {
+        return <Square onClick={() => increment(n)} n={n} key={n}/>
+      })}
     </div>
   )
 }
