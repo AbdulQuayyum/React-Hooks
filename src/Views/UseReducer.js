8ef0235d72617426caf53dd5e860ac87998cb979
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-07 12:19:11 +0100

    Cooking👨🏾‍🍳...

--- src/Views/UseReducer.js
+++ src/Views/UseReducer.js
@@ -42,8 +42,10 @@ function reducer(state, action) {
   switch (action.type) {
     case "add-todo":
       return { todos: [...state.todos, { text: action.text, completed: false }] }
-    case "remove-todo":
-      return
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((t, index) => index === action.index ? { ...t, completed: !t.completed } : t)
+      }
     default:
       return state
   }
@@ -84,8 +86,13 @@ const UseReducer = () => {
         </button>
       </form>
       {/* <pre> {JSON.stringify(todos, null, 2)} </pre> */}
-      {todos.map(t => (
-        <div key={t.text}> {t.text} </div>
+      {todos.map((t, index) => (
+        <div
+          key={t.text}
+          style={{ textDecoration: t.completed ? "line-through" : "" }}
+          onClick={() => dispatch({ type: "toggle-todo", index })}  >
+          {t.text}
+        </div>
       ))}
     </div>
   )
